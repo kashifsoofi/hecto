6cf9e357f028ae55c5ec7a93f858f6370ee71500
Kashif Soofi <kashif.soofi+git@gmail.com>
2023-09-19 12:27:45 +0100

    Update to use crossterm

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,15 @@
-use std::io::{self, stdout, Read};
-use termion::raw::IntoRawMode;
+//importing in execute! macro
+// #[macro_use]
+// extern crate crossterm;
+
+use std::io::{self, Read};
+use crossterm::{
+    event::{
+        read, Event, KeyCode, KeyEvent, KeyModifiers, KeyEventKind, KeyEventState,
+    },
+    // style::Print,
+    terminal::{disable_raw_mode, enable_raw_mode, Clear, ClearType},
+};
 
 fn to_ctrl_byte(c: char) -> u8 {
     let byte = c as u8;
@@ -8,10 +18,19 @@ fn to_ctrl_byte(c: char) -> u8 {
 
 fn die(e: std::io::Error) {
     panic!("{}", e);
-} 
+}
+
+struct Cleanup;
+
+impl Drop for Cleanup {
+    fn drop(&mut self) {
+        disable_raw_mode().expect("Unable to disable raw mode");
+    }
+}
 
 fn main() {
-    let _stdout = stdout().into_raw_mode().unwrap();
+    let _cleanup = Cleanup;
+    enable_raw_mode().expect("Failed to enable raw mode");
 
     for b in io::stdin().bytes() {
         match b {
